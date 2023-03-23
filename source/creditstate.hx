b676b87158ff34eb11d956befca35670b8f73c7c
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-03-23 16:23:46 -0400

    idk wtf im coding
    this engine is shit, why tf did you make it
    
    delete it

---
+++ source/creditstate.hx
@@ -0,0 +1,40 @@
+package;
+
+class creditstate { //name, icon, role, discription, color ( colors: 1 - red, 2 - orange, 3 - yellow, 4 - green, 5 - blue, 6 - purple, 7 - pink, 8 - other ( random color )).
+public static var creditSHIT:Array<Dyamic> = [
+  ["OakShubs", "shubs", "creator", "Creator of this engine", "7"],
+  ["MyFnf", "fnf", "creator", "Programmer of this engine", "5"]
+  
+  add(creditSHIT);
+  
+  text = 'vcr.ttf';
+  colors {
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8
+  
+     };
+  
+  color.output {
+  
+  1 = red,
+  2 = orange,
+  3 = yellow,
+  4 = green,
+  5 = blue,
+  6 = purple,
+  7 = pink,
+  8 = string('1 - 7', 'random');
+  
+     }
+  ]
+}
+ 
+  
+  
+  
